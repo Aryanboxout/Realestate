da212550af0761be09ff5a0136f8a28ec6ac3ae7
vaishavijay <85912486+vaishavijay@users.noreply.github.com>
2021-09-02 12:56:18 -0700

    This is Vaishavi's commit

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
 
 
-# connects default URL to render index.html
+# connects default URL of server to render kangaroos.html
 @app.route('/')
 def index():
-    return render_template("index.html")
-
+        return render_template("index.html")
 
 # connects /kangaroos path to render kangaroos.html
 @app.route('/kangaroos/')
@@ -17,11 +16,13 @@ def kangaroos():
     return render_template("kangaroos.html")
 
 
+# connects /walruses path to render walruses.html
 @app.route('/walruses/')
 def walruses():
     return render_template("walruses.html")
 
 
+# connects /walruses path to render walruses.html
 @app.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
@@ -31,6 +32,16 @@ def hawkers():
 def stub():
     return render_template("stub.html")
 
+
+@app.route('/vai/', methods=['GET', 'POST'])
+def vai():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("vai.html", name1=name)
+    # starting and empty input default
+    return render_template("vai.html", name1="World")
 
 # runs the application on the development server
 if __name__ == "__main__":
