c33da1ef6925e4c6a525e787507d49072f0e0bc4
arushi1010 <arushi9b@gmail.com>
2021-09-02 15:43:47 -0700

    Arushi's Commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import "packages" from flask
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
@@ -27,9 +28,15 @@ def hawkers():
     return render_template("hawkers.html")
 
 
-@app.route('/stub/')
-def stub():
-    return render_template("stub.html")
+@app.route('/arushi', methods=['GET', 'POST'])
+def arushi():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("arushi.html", name1=name)
+    # starting and empty input default
+    return render_template("arushi.html", name1="World")
 
 
 # runs the application on the development server
