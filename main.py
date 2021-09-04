356a7e2d914795fe599b09f4afe3408c7182898f
PrishaB <prishab.1220@gmail.com>
2021-09-03 18:35:56 -0700

    Prisha's commit - updated main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-# connects default URL to render index.html
+# connects default URL of server to render kangaroos.html
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -17,23 +17,53 @@ def kangaroos():
     return render_template("kangaroos.html")
 
 
+# connects /walruses path to render walruses.html
 @app.route('/walruses/')
 def walruses():
     return render_template("walruses.html")
 
 
+# connects /walruses path to render walruses.html
 @app.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
 
 
-@app.route('/stub/')
-def stub():
-    return render_template("stub.html")
+@app.route('/arushi/', methods=['GET', 'POST'])
+def arushi():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("arushi.html", name1=name)
+    # starting and empty input default
+    return render_template("arushi.html", name1="World")
+
+
+@app.route('/prisha/', methods=['GET', 'POST'])
+def prisha():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("prisha.html", name1=name)
+    # starting and empty input default
+    return render_template("prisha.html", name1="World")
 
 
 @app.route('/vai/', methods=['GET', 'POST'])
 def vai():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("vai.html", name1=name)
+    # starting and empty input default
+    return render_template("vai.html", name1="World")
+
+
+@app.route('/siya/', methods=['GET', 'POST'])
+def siya():
     # submit button has been pushed
     if request.form:
         name = request.form.get("name")
