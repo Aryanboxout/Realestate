40fb2e89ae3b3b75d10aec8faf315419f206f405
PrishaB <prishab.1220@gmail.com>
2021-09-09 13:04:11 -0700

    Prisha's commit - fixed base.html

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
+
 # create a Flask instance
 app = Flask(__name__)
+
+
 # connects default URL of server to render kangaroos.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
-    return render_template("index.html")
 
 # connects /kangaroos path to render kangaroos.html
 @app.route('/kangaroos/')
 def kangaroos():
     return render_template("kangaroos.html")
+
+
 # connects /walruses path to render walruses.html
 @app.route('/walruses/')
 def walruses():
     return render_template("walruses.html")
+
+
 # connects /walruses path to render walruses.html
 @app.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
+
 @app.route('/arushi/', methods=['GET', 'POST'])
 def arushi():
     # submit button has been pushed
@@ -30,6 +38,8 @@ def arushi():
             return render_template("arushi.html", name1=name)
     # starting and empty input default
     return render_template("arushi.html", name1="World")
+
+
 @app.route('/prisha/', methods=['GET', 'POST'])
 def prisha():
     # submit button has been pushed
@@ -39,6 +49,8 @@ def prisha():
             return render_template("prisha.html", name1=name)
     # starting and empty input default
     return render_template("prisha.html", name1="World")
+
+
 @app.route('/vai/', methods=['GET', 'POST'])
 def vai():
     # submit button has been pushed
@@ -63,4 +75,4 @@ def siya():
 
 # runs the application on the development server
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080)
+    app.run(debug=True)
