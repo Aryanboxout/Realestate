b2e7ba481ea4b103f6aa4d9b217b849ac9db3699
arushi1010 <arushi9b@gmail.com>
2021-09-17 13:54:08 -0700

    Arushi's Commit - Binary Code with Working Text Box

--- main.py
+++ main.py
@@ -73,9 +73,14 @@ def siya():
     return render_template("siya.html", name1="World")
 
 
-@app.route('/binary/')
+@app.route("/binary", methods=['GET','POST'])
 def binary():
-    return render_template("binary.html")
+    if request.form:
+        bits = request.form.get("bits")
+        if len(bits) != 0:  # input field has content
+            return render_template("binary.html", bits=int(bits))
+        # starting and empty input default
+    return render_template("binary.html", bits=8)
 
 
 @app.route('/mini_labs/')
